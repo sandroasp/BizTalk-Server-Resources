5a8578cb9eab942c81335f08a46e153c63375ed4
Sandro Pereira <sandro-pereira@live.com.pt>
2020-03-26 14:25:51 +0000

    Export BizTalk Server Receive Port Binding with PowerShell

---
+++ PowerShell-scripts/bts-receive-port-exportbindings/README.md
@@ -0,0 +1,12 @@
+# Export BizTalk Server Receive Port Binding with PowerShell
+With this PowerShell sample we will be able to generate a binding file for a specific assembly name deployed in my BizTalk Server environment:
+* Generate a Binding file for 3 environments DEV, QA and PRD:
+* Changing the NT Group Name for each different environment;
+* Generate a specific Receive Port deployed in your environement 
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Us
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
+
+**Pedro Almeida** | [DevScope](http://www.devscope.net/) | Integration Senior Developer
---
+++ PowerShell-scripts/bts-receive-port-exportbindings/bts-receive-port-exportbindings.ps1
@@ -0,0 +1,39 @@
+﻿# Set Global Variables
+# NT Group Name configure to run orchestrations, normally "BizTalk Application Users"
+[string]$global:qaNTGroupName = 'DOMAIN\BizTalk QA Application Users'
+[string]$global:prdNTGroupName = 'DOMAIN\BizTalk PRD Application Users'
+
+function bts-receive-port-exportbindings([string]$bindingFilePath, [string]$appName, [string]$portName, [boolean]$generateDiffEnvBindings)
+{
+    $portRen = $portName.Replace(" ", "")
+
+    $taskParams = ” ExportBindings /Destination:$bindingfilePath$appName.$portRen.BindingInfo.xml /ApplicationName:$appName ”
+    #First version: $p = [diagnostics.process]::start(“BTSTask.exe”, $taskParams)
+    Start-Process "BTSTask.exe" $taskParams -Wait
+
+    $xml = [xml](Get-Content "$bindingfilePath$appName.$portRen.BindingInfo.xml")
+    foreach($RemoveModuleRef in $xml.BindingInfo.ModuleRefCollection.ModuleRef)
+    {
+        $xml.BindingInfo.ModuleRefCollection.RemoveChild($RemoveModuleRef)
+    }
+    foreach($RemoveSendPort in $xml.BindingInfo.SendPortCollection.SendPort)
+    {
+        $xml.BindingInfo.SendPortCollection.RemoveChild($RemoveSendPort)
+    }
+    foreach($RemoveReceivePort in $xml.BindingInfo.ReceivePortCollection.ReceivePort)
+    {
+        if($RemoveReceivePort.Name -ne $portName)
+        {
+            $xml.BindingInfo.ReceivePortCollection.RemoveChild($RemoveReceivePort)
+        }
+    }
+    $xml.Save("$bindingfilePath$appName.$portRen.BindingInfo.xml")
+
+    if($generateDiffEnvBindings)
+    {
+        $xml.Save("$bindingfilePath$appName.$portRen.QA.BindingInfo.xml")
+        $xml.Save("$bindingfilePath$appName.$portRen.PRD.BindingInfo.xml")
+    }
+}
+
+bts-receive-port-exportbindings 'C:\temp\BTS\' 'BizTalk Application 1' 'MY_SCH_PORT' $True
