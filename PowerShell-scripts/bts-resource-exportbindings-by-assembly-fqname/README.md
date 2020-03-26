70c9e87fba281af67d7cf5abec854d792d88079d
Sandro Pereira <sandro-pereira@live.com.pt>
2020-03-26 14:33:21 +0000

    Export BizTalk Server Resource Bindings by Assembly FQName with PowerShell

---
+++ PowerShell-scripts/bts-resource-exportbindings-by-assembly-fqname/README.md
@@ -0,0 +1,13 @@
+# Export BizTalk Server Resource Bindings by Assembly FQName with PowerShell
+Exporting a BizTalk Server Resource Bindings by Assembly fully qualified name, similar to the exporting Application Binding, is, at first sight, a simple and quick task that can be done using the BizTalk Server Administration Console. But even in simple tasks we may encounter challenges that require us to perform some monotonous and boring manual operations that consume some of our precious time and are always subject to failures.
+
+With this PowerShell sample for a specific assembly fully qualified name deployed in my BizTalk Server environemnt I can easily:
+* Generate a Binding file for 3 environments DEV, QA and PRD:
+* Changing the NT Group Name for each different environment;
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Us
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
+
+**Pedro Almeida** | [DevScope](http://www.devscope.net/) | Integration Senior Developer
---
+++ PowerShell-scripts/bts-resource-exportbindings-by-assembly-fqname/bts-resource-exportbindings-by-assembly-fqname.ps1
@@ -0,0 +1,30 @@
+﻿# Set Global Variables
+# NT Group Name configure to run orchestrations, normally "BizTalk Application Users"
+[string]$global:qaNTGroupName = 'DOMAIN\BizTalk QA Application Users'
+[string]$global:prdNTGroupName = 'DOMAIN\BizTalk PRD Application Users'
+
+function bts-resource-exportbindings-by-assembly-fqname([string]$bindingFilePath, [string]$appName, [string]$assemblyFQName, [boolean]$generateDiffEnvBindings)
+{
+    $dllName = $assemblyFQName.Substring(0, $assemblyFQName.IndexOf(','))
+    $taskParams = ” ExportBindings /Destination:$bindingfilePath$appName.$dllName.BindingInfo.xml /AssemblyName:""$assemblyFQName"" ”
+    Start-Process "BTSTask.exe" $taskParams -Wait
+
+    if($generateDiffEnvBindings)
+    {
+        $xml = [xml](Get-Content "$bindingfilePath$appName.$dllName.BindingInfo.xml")
+    
+        # QA Binding Info Generation
+        $xml.SelectNodes("//Host") | % { 
+            $_.NtGroupName = $global:qaNTGroupName
+        }
+        $xml.Save("$bindingfilePath$appName.$dllName.QA.BindingInfo.xml")
+
+        # PRD Binding Info Generation
+        $xml.SelectNodes("//Host") | % { 
+            $_.NtGroupName = $global:prdNTGroupName
+        }
+        $xml.Save("$bindingfilePath$appName.$dllName.PRD.BindingInfo.xml")
+    }
+}
+
+bts-resource-exportbindings-by-assembly-fqname 'C:\temp\BTS\' 'Pmc.Feiserver' 'TriggerPollingPOC, Version=1.0.0.0, Culture=neutral, PublicKeyToken=eebb1a3d8ea686f3' $True
