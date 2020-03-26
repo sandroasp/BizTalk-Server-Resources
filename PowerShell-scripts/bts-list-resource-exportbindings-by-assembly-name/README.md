1cdb10941d399e7932e22119b2fd5d0f4357280a
Sandro Pereira <sandro-pereira@live.com.pt>
2020-03-26 12:21:10 +0000

    Export BizTalk Resource Bindings from a List of Assembly Names with PowerShell

---
+++ PowerShell-scripts/bts-list-resource-exportbindings-by-assembly-name/README.md
@@ -0,0 +1,12 @@
+# Export BizTalk Resource Bindings from a List of Assembly Names with PowerShell
+With this PowerShell sample we will be able to generate a unique binding file for a list of specific assemblies name deployed in my BizTalk Server environment:
+* Generate a Binding file for 3 environments DEV, QA and PRD:
+* Changing the NT Group Name for each different environment;
+* Generate an unique binding file, instead of having separeted binding files for each assembly;
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About US
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
+
+**Pedro Almeida** | [DevScope](http://www.devscope.net/) | Integration Senior Developer
---
+++ PowerShell-scripts/bts-list-resource-exportbindings-by-assembly-name/bts-list-resource-exportbindings-by-assembly-name.ps1
@@ -0,0 +1,98 @@
+﻿# Set Global Variables
+# NT Group Name configure to run orchestrations, normally "BizTalk Application Users"
+[string]$global:qaNTGroupName = 'DOMAIN\BizTalk QA Application Users'
+[string]$global:prdNTGroupName = 'DOMAIN\BizTalk PRD Application Users'
+
+function bts-list-resource-exportbindings-by-assembly-name([string]$bindingFilePath, [string]$appName, [string]$listAssemblyName, [boolean]$generateDiffEnvBindings) 
+{ 
+    #splits all the assemblies by | 
+    $list = $listAssemblyName.Split("|")  
+    $finalBinding = [xml](Get-Content "C:\Temp\TemplateBindingInfo.xml") 
+    $moduleRefNode = $finalBinding.SelectSingleNode("BindingInfo/ModuleRefCollection") 
+    $sendPortNode = $finalBinding.SelectSingleNode("BindingInfo/SendPortCollection") 
+    $receivePortNode = $finalBinding.SelectSingleNode("BindingInfo/ReceivePortCollection") 
+    $displayName = 'assemblyName' 
+    $appsList=New-Object System.Collections.ArrayList 
+    $assemblyListFQN = New-Object System.Collections.ArrayList      
+    $appsList = BTSTask.exe ListApp /ApplicationName:$appName 
+    #region Add FQN to list 
+    foreach($string in $appsList) 
+    {          
+        #region Get Assembly Fully Qualified Name 
+        $list = $listAssemblyName.Split("|") 
+                     
+        foreach($element in $list) 
+        {  
+            if($string.Contains('-')) 
+            { 
+                if($string.Split('-')[1].Split('"')[1] -eq "System.BizTalk:BizTalkAssembly") 
+                { 
+                    foreach($element in $list){       
+                        if($string.Split('-')[2].Split('"')[1].StartsWith($element)) 
+                        { 
+                            if(!$assemblyListFQN.Contains($string.Split('-')[2].Split('"')[1])){ 
+                                $assemblyListFQN.Add($string.Split('-')[2].Split('"')[1]); 
+                                #display name Set 
+                            } 
+                        } 
+                    } 
+                } 
+            } 
+        } 
+        #endregion    
+    } 
+    #endregion 
+     
+    #loop assemblies 
+    foreach($string in $assemblyListFQN) 
+    {         
+        $dllName = $string.Substring(0, $string.IndexOf(',')) 
+        $taskParams = ” ExportBindings /Destination:$bindingfilePath$appName.$dllName.BindingInfo.xml /AssemblyName:""$string"" ” 
+        Start-Process "BTSTask.exe" $taskParams -Wait 
+ 
+        $xml = [xml](Get-Content "$bindingfilePath$appName.$dllName.BindingInfo.xml") 
+ 
+        foreach($moduleRef in $xml.BindingInfo.ModuleRefCollection.ModuleRef) 
+        { 
+            $node = $finalBinding.ImportNode(($moduleRef), $true); 
+            $moduleRefNode.AppendChild($node); 
+        } 
+        foreach($sendPort in $xml.BindingInfo.SendPortCollection.SendPort) 
+        { 
+            $node = $finalBinding.ImportNode(($sendPort), $true); 
+            $sendPortNode.AppendChild($node); 
+        } 
+        foreach($receivePort in $xml.BindingInfo.ReceivePortCollection.ReceivePort) 
+        { 
+            $node = $finalBinding.ImportNode(($receivePort), $true); 
+            $receivePortNode.AppendChild($node); 
+        } 
+    } 
+    $finalBinding.Save("$bindingfilePath$appName.BindingInfo.xml") 
+ 
+    #generate different Environments Bindings 
+    if($generateDiffEnvBindings) 
+    { 
+        $xml = [xml](Get-Content "$bindingfilePath$appName.BindingInfo.xml") 
+     
+        # QA Binding Info Generation 
+        $xml.SelectNodes("//Host") | % {  
+            if(!$_.Attributes['xsi:nil'].Value) 
+            { 
+                $_.NtGroupName = $global:qaNTGroupName 
+            } 
+        } 
+        $xml.Save("$bindingfilePath$appName.QA.BindingInfo.xml") 
+ 
+        # PRD Binding Info Generation 
+        $xml.SelectNodes("//Host") | % {  
+            if(!$_.Attributes['xsi:nil'].Value) 
+            { 
+                $_.NtGroupName = $global:prdNTGroupName 
+            } 
+        } 
+        $xml.Save("$bindingfilePath$appName.PRD.BindingInfo.xml") 
+    } 
+}
+
+bts-list-resource-exportbindings-by-assembly-name 'C:\temp\BTS\' 'BizTalk Application 1' 'Demo' $True
