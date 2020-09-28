25386ed250318b221332775da1da05bac29735c9
Sandro Pereira <sandro-pereira@live.com.pt>
2020-09-28 22:27:34 +0100

    URI Update

---
+++ PowerShell-scripts/BTS-Update-URI-ReceiveLocations/BTS-Update-URI-ReceiveLocations.ps1
@@ -0,0 +1,51 @@
+﻿##############################################################################################
+# Script to properly update the URI (address) or part of the URI on a list of
+# Receive locations present in the environment.
+#
+# Author: Sandro Pereira
+##############################################################################################
+
+[System.reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM") | Out-Null
+
+#############################################################
+# Global Variables
+#############################################################
+[string] $bizTalkDbServer = "SQLSERVER"
+[string] $bizTalkDbName = "BizTalkMgmtDb"
+$rcvLocations = "RECEIVE_LOCATION_1","RECEIVE_LOCATION_2","RECEIVE_LOCATION_3"
+
+#############################################################
+# Get information from BizTalk DV
+#############################################################
+$catalog = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
+$catalog.ConnectionString = "SERVER=$bizTalkDbServer;DATABASE=$bizTalkDbName;Integrated Security=SSPI"
+
+#############################################################
+# Update URI
+#############################################################
+foreach($receivePort in $catalog.ReceivePorts)
+{
+    # For each receive location in your environment
+    foreach($recLocation in $receivePort.ReceiveLocations)
+    {
+        # In this case ...
+        if($rcvLocations.Contains($recLocation.Name))
+        {
+            [string] $address = $recLocation.Address
+            $address = $address.Replace("DEV-SERVER-NAME","PRO_SERVER-NAME")
+            # Sample of additional custom changes
+            if($address.Contains("PREFIX"))
+            {
+                $address = $address.Replace("DATABASE","DATABASE-WITH-PREFIX-A")
+            }
+            else
+            {
+                $address = $address.Replace("DATABASE","DATABASE-WITH-PREFIX-B")
+            }
+            $recLocation.Address = $address
+        }
+    }
+}
+
+$catalog.SaveChanges()
+$catalog.Refresh()
---
+++ PowerShell-scripts/BTS-Update-URI-ReceiveLocations/README.md
@@ -0,0 +1,7 @@
+# Update URI on BizTalk Server Receive Locations with PowerShell
+With this PowerShell sample we will be able to set or update the URI (address) or part of the URI on a lisf of BizTalk Server Receive Locationsa deployed in your BizTalk Server environment.
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Us
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
