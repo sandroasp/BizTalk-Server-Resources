069de7c45b597da9ec081ebde8802b785bded1ae
Sandro Pereira <sandro-pereira@live.com.pt>
2020-09-28 18:28:36 +0100

    credentials

---
+++ PowerShell-scripts/BTS-Update-Credentials-ReceiveLocations/BTS-Update-Credentials-ReceiveLocations.ps1
@@ -0,0 +1,59 @@
+﻿##############################################################################################
+# Script to properly update the credentials (username and password) on a list of 
+# Receive locations present in the environment.
+#
+# Author: Sandro Pereira
+##############################################################################################
+
+[System.reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM") | Out-Null
+
+#############################################################
+# Global Variables
+#############################################################
+[string] $bizTalkDbServer = "SQLSERVER"
+[string] $bizTalkDbName = "BizTalkMgmtDb"
+$rcvLocations = "RECEIVE_LOCATION_1","RECEIVE_LOCATION_2","RECEIVE_LOCATION_3"
+
+#############################################################
+# Get DB info
+#############################################################
+$catalog = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
+$catalog.ConnectionString = "SERVER=$bizTalkDbServer;DATABASE=$bizTalkDbName;Integrated Security=SSPI"
+
+#############################################################
+# Update Credentials
+#############################################################
+foreach($receivePort in $catalog.ReceivePorts)
+{
+    # For each receive location in your environment
+    foreach($recLocation in $receivePort.ReceiveLocations)
+    {
+        # In this case ...
+        if($rcvLocations.Contains($recLocation.Name))
+        {
+            [xml]$bindingConfiguration = $recLocation.TransportTypeData
+            if($bindingConfiguration.CustomProps.Password.vt -eq "1")
+            {
+                $bindingConfiguration.CustomProps.Password.InnerText = "my_password"
+                $bindingConfiguration.CustomProps.Password.vt = "8"
+            }
+            else
+            {
+                $passwordElement = $bindingConfiguration.CreateElement("Password")
+                $passwordElement.SetAttribute("vt", "8")
+                $passwordElement.InnerText = "my_password"
+                $bindingConfiguration.CustomProps.InsertAfter($passwordElement, $bindingConfiguration.CustomProps.SuspendMessageOnFailure)
+            }
+            if($bindingConfiguration.CustomProps.UserName.vt -eq "8")
+            {
+                $bindingConfiguration.CustomProps.UserName.InnerText = "my_username"
+            }
+
+            $transportConfigData = $bindingConfiguration.InnerXml
+            $recLocation.TransportTypeData = $transportConfigData
+        }
+    }
+}
+
+$catalog.SaveChanges()
+$catalog.Refresh()
---
+++ PowerShell-scripts/BTS-Update-Credentials-ReceiveLocations/README.md
@@ -0,0 +1,7 @@
+# Set Authentication Credential on BizTalk Server Receive Locations with PowerShell
+With this PowerShell sample we will be able to set or update Authentication Credential on a lisf of BizTalk Server Receive Locationsa deployed in your BizTalk Server environment.
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Us
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
