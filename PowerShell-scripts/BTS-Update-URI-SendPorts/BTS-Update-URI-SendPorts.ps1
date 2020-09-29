9997787f588f64e1623610d3edee591f7b01ef0c
Sandro Pereira <sandro-pereira@live.com.pt>
2020-09-29 22:32:05 +0100

    Update URI on BizTalk Server Send Ports with PowerShell

---
+++ PowerShell-scripts/BTS-Update-URI-SendPorts/BTS-Update-URI-SendPorts.ps1
@@ -0,0 +1,43 @@
+﻿##############################################################################################
+# Script to properly update the URI (address) or part of the URI on a list of
+# Send Ports present in the environment.
+#
+# Author: Sandro Pereira
+##############################################################################################
+
+[System.reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM") | Out-Null
+
+#############################################################
+# Global Variables
+#############################################################
+[string] $bizTalkDbServer = "BTS2020LAB01"
+[string] $bizTalkDbName = "BizTalkMgmtDb"
+$sndPorts = "SendPort2","SQL_UPDATEDEMO_A_SENDPORT"
+
+#############################################################
+# Get information from BizTalk DV
+#############################################################
+$catalog = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
+$catalog.ConnectionString = "SERVER=$bizTalkDbServer;DATABASE=$bizTalkDbName;Integrated Security=SSPI"
+
+foreach($SendPort in $catalog.SendPorts)
+{
+    # In this case ...
+    if($sndPorts.Contains($SendPort.Name))
+    {
+        [string] $address = $SendPort.PrimaryTransport.Address
+
+        if($address.Contains("mssql://"))
+        {
+            $address = $address.Replace("mssql://.","mssql://BTS2020LAB01")
+        }
+        else
+        {
+            $address = $address.Replace("C:\","D:\")
+        }
+        $SendPort.PrimaryTransport.Address = $address
+    }
+}
+
+$catalog.SaveChanges()
+$catalog.Refresh()
---
+++ PowerShell-scripts/BTS-Update-URI-SendPorts/README.md
@@ -0,0 +1,7 @@
+# Update URI on BizTalk Server Send Ports with PowerShell
+With this PowerShell sample we will be able to set or update the URI (address) or part of the URI on a list of BizTalk Server Send Ports deployed in your BizTalk Server environment.
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Us
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
