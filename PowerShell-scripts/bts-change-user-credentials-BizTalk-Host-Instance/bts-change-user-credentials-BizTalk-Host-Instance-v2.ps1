4e2b1c0a996369ba957df6077fea3cd057678c43
Sandro Pereira <sandro-pereira@live.com.pt>
2022-01-21 11:37:26 +0000

    Change the startup type for BizTalk services to Automatic (Delayed Start) with PowerShell

---
+++ PowerShell-scripts/bts-change-user-credentials-BizTalk-Host-Instance/bts-change-user-credentials-BizTalk-Host-Instance-v2.ps1
@@ -0,0 +1,11 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: This script will allow you to change the user credentials of a specific Host Instance   #
+#                                                                                                      #
+########################################################################################################
+
+$hostApp = gwmi -n 'root/MicrosoftBizTalkServer' -q 'select * from MSBTS_HostInstance where HostName="BiztalkDemoApplication"'
+  
+$hostApp.Install("DOMAIN\User", "password", "true")
---
+++ PowerShell-scripts/bts-change-user-credentials-BizTalk-Host-Instance/bts-change-user-credentials-BizTalk-Host-Instance.ps1
@@ -0,0 +1,19 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: This script will allow you to change the user credentials of a specific Host Instance   #
+#                                                                                                      #
+########################################################################################################
+
+# Getting the list of BizTalk Host Instances objects
+$hosts = Get-WmiObject MSBTS_HostInstance -namespace 'root/MicrosoftBizTalkServer'
+  
+# Listing existing BizTalk Host Instances names
+$hosts | ft HostName
+ 
+# Getting/Filter the desired instance
+$MyHost = $hosts | ?{$_.HostName -eq "BizTalkDemoApplication"}
+ 
+# Change BizTalk Host Instance user credentials
+$MyHost.Install("DOMAIN\User", "password", "true")
