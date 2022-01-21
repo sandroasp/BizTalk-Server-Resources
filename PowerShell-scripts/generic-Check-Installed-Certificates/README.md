fa5006ae23eecb398dc7b9a1619173b7d9e8ccaa
Sandro Pereira <sandro-pereira@live.com.pt>
2022-01-21 12:29:39 +0000

    Check were a specific certificated is installed with PowerShell

---
+++ PowerShell-scripts/generic-Check-Installed-Certificates/README.md
@@ -0,0 +1,8 @@
+# Check were a specific certificated is installed with PowerShell
+
+These PowerShell scripts will allow you to get information were a specific certificated is installed on the machine by search by it's thumbprint.
+ 
+THESE POWERSHELL SCRIPTS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Us
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
---
+++ PowerShell-scripts/generic-Check-Installed-Certificates/generic-Check-Installed-Certificates-v2.ps1
@@ -0,0 +1,9 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira & José Anónio Silva                                                           #
+#                                                                                                      #
+# Description: This script will get information were a specific certificated is installed              #
+#                                                                                                      #
+########################################################################################################
+
+gci cert:\* -Recurse | ?{$_.Thumbprint -eq "5693ae76acfe33325bd6e1f05f38a9941892cb69"} | select PSParentPath
