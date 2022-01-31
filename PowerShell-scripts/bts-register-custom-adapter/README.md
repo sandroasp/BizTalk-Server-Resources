64147dd2f8d7ac0e7787aaf35a6473c2c321f1ec
Sandro Pereira <sandro-pereira@live.com.pt>
2022-01-31 21:37:19 +0000

    PowerShell Script to Register Custom Adapters

---
+++ PowerShell-scripts/bts-register-custom-adapter/README.md
@@ -0,0 +1,7 @@
+# PowerShell Script to Register Custom Adapters
+This PowerShell script will ask you the directory and name of the Windows Registry file (.reg), and after validating its existence, it will ask you if you want to register to run under a 32-bit or/and 64-bit host instances.
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
---
+++ PowerShell-scripts/bts-register-custom-adapter/bts-register-custom-adapter.ps1
@@ -0,0 +1,58 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: This script register a custom adapter to run under 32-bit and/or 64-bit Host Instances. #
+#                                                                                                      #
+########################################################################################################
+
+function Register-BizTalk-Adapter(
+	[string]$adapterRegFolder,
+	[string]$regFile)
+{
+    [string]$regFilePath = $adapterRegFolder + "\" + $regFile
+
+    #If the file does not exist, create it.
+    if (-not(Test-Path -Path $regFilePath -PathType Leaf)) {
+        Write-Host "The file [$regFilePath] does not exist." -Fore Red
+        return $false
+    }
+    
+    #Test for 64 bit
+    $questionResult = $windowsShell.popup("Do you want to install this adapter to run under 64-bit Host Instances?", 0,"64-bit Host Instances installation",4)
+
+    If ($questionResult -eq 6) {
+        # c:\windows\regedit.exe /S $regFilePath
+        reg import $regFilePath /reg:64
+        Write-Host "Adapter installed as 64-bit." -Fore DarkGreen
+    }
+
+    $questionResult = $windowsShell.popup("Do you want to install this adapter to run under 32-bit Host Instances?", 0,"32-bit Host Instances installation",4)
+
+    If ($questionResult -eq 6) {
+        # c:\Windows\syswow64\regedit /S $regFilePath
+        reg import $regFilePath /reg:32
+        Write-Host "Adapter installed as 32-bit." -Fore DarkGreen
+    }
+}
+
+#############################################################
+# Main script logic
+#############################################################
+Write-Host "Starting registring a new adapter into BizTalk Server environment..." -Fore DarkGreen
+
+# STEP 1 
+# Ask for the path on the hard drive where the adapter and the .ref file for that adapter are installed 
+[string]$pathReg = Read-Host -Prompt "Please enter path where the adapter and .reg file is intalled (C:\Program Files (x86)\adapter)" 
+
+# STEP 2 
+# Ask for the name of the adapter .ref file  
+[string]$regFile = Read-Host -Prompt "Please enter name of the adapter .reg file (StaticAdapterManagement.reg)"
+
+# STEP 3
+# trigger function Register-BizTalk-Adapter
+Register-BizTalk-Adapter $pathReg $regFile
+
+# STEP 4
+# Finish
+Write-Host "Adapter registration completed" -Fore DarkGreen
