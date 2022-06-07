f49426bde0e972c224d5b93b794c2e4c97ad781e
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-07 17:10:33 +0100

    MSBTS_Host

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_Host/MSBTS_Host.ps1
@@ -0,0 +1,11 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: USe of the MSBTS_Host WMI Class.                                                        #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+# Get the tracking host information
+$trackingHost = Get-WmiObject MSBTS_Host -Namespace root\MicrosoftBizTalkServer -ErrorAction Stop | where {$_.HostTracking -eq "true" }
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_Host/README.md
@@ -0,0 +1,7 @@
+# PowerShell to get informations about the tracking host 
+In this PowerShell sample we are getting the details information about the tracking host(s)
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
