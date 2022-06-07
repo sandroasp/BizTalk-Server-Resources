9bcb041423579fdf3252ca29a9da5c0323073a77
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-07 16:45:25 +0100

    MSBTS_GroupSetting

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_GroupSetting/MSBTS_GroupSetting.ps1
@@ -0,0 +1,45 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: USe of the MSBTS_GroupSetting WMI Class.                                                #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+# Get BizTalk Server DB info
+# Set name of MgmtDbServer
+$global:mgmtDbServer = get-wmiobject MSBTS_GroupSetting -namespace root\MicrosoftBizTalkServer | select-object -expand MgmtDbServerName
+
+# Set via WMI name of MgmtDb
+$global:mgmtDbName = get-wmiobject MSBTS_GroupSetting -namespace root\MicrosoftBizTalkServer | select-object -expand MgmtDbName
+
+
+
+function Connect-BTSCatalog {
+    [CmdletBinding()]
+    [OutputType([Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer])]
+    Param (
+        [Parameter()]
+        [string]$Server = $env:COMPUTERNAME,
+        [Parameter()]
+        [string]$SQLInstance,
+        [Parameter()]
+        [string]$ManagementDB
+    )
+    Process {        
+        #Calculate BizTalk connection string
+        $wmi = Get-WmiObject -Class MSBTS_GroupSetting -Namespace root\MicrosoftBizTalkServer -computer $server -ErrorAction Stop
+        if (-not $SQLInstance) {
+            $SQLInstance = $wmi.MgmtDbServerName
+        }
+        if (-not $ManagementDB) {
+            $ManagementDB = $wmi.MgmtDbName
+        }
+
+        $btsCatalog = New-Object "Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer"
+        $btsCatalog.ConnectionString = "SERVER=$SQLInstance;DATABASE=$ManagementDB;Integrated Security=SSPI"
+
+        return $btsCatalog
+    }
+}
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_GroupSetting/README.md
@@ -0,0 +1,7 @@
+# PowerShell to get informations about BizTalk Server databases 
+In this PowerShell sample we are getting informations about BizTalk Server databases and a function to Get BizTalk Server Catalog
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
