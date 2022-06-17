45246bf87191b7d7d992ffccfd2a99fea0ab7b6d
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-18 00:42:12 +0100

    MSBTS_HostInstance

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_HostInstance/MSBTS_HostInstance.ps1
@@ -0,0 +1,50 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Sample use of the MSBTS_HostInstance  WMI Class.                                        #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+function Disable-HostInstances
+{
+    $hostInstances = Get-WmiObject MSBTS_HostInstance -Namespace root\MicrosoftBizTalkServer -Filter HostType=1
+
+    foreach ($hostInstance in $hostinstances)
+    {
+        $hostInstance.IsDisabled = $true
+        $hostInstance.Put() > $null
+    }
+
+    $hostInstances.Stop()
+}
+
+function Enable-HostInstances
+{
+    $hostInstances = Get-WmiObject MSBTS_HostInstance -Namespace root\MicrosoftBizTalkServer -Filter HostType=1
+
+    foreach ($hostInstance in $hostinstances)
+    {
+        $hostInstance.IsDisabled = $false
+        $hostInstance.Put() > $null
+    }
+
+    $hostInstances.Start()
+}
+
+function Stop-HostInstances
+{
+    $hostInstances = Get-WmiObject MSBTS_HostInstance -Namespace root\MicrosoftBizTalkServer -Filter HostType=1
+
+    $hostInstances.Stop()
+}
+
+function Start-HostInstances
+{
+    $hostInstances = Get-WmiObject MSBTS_HostInstance -Namespace root\MicrosoftBizTalkServer -Filter HostType=1
+
+    $hostInstances.Start()
+}
+
+Start-HostInstances
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_HostInstance/README.md
@@ -0,0 +1,7 @@
+# PowerShell to Disable and Enable Host Instance and Start and Stop them
+In this PowerShell sample we are using it to Disable and Enable Host Instance and Start and Stop them
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
