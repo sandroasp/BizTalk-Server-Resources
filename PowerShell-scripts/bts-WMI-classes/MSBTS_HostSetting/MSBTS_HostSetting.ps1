5a5b7d99009a085e534efc4ece65a9e8caa47f9b
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-19 00:39:21 +0100

    MSBTS_HostSetting

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_HostSetting/MSBTS_HostSetting.ps1
@@ -0,0 +1,11 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: USe of the MSBTS_HostSetting WMI Class.                                                 #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+# Get Host Setting
+$hSettings = Get-WmiObject MSBTS_HostSetting -Namespace root\MicrosoftBizTalkServer | where {$_.Name -eq "BizTalkServerApplication" }
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_HostSetting/README.md
@@ -0,0 +1,7 @@
+# PowerShell to Get Host Settings
+In this PowerShell sample we are getting the BizTalkServerApplication host settings
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
