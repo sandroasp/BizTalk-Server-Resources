fd82e93603ff28ac7405b6ec07d39443254e34eb
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-20 19:45:59 +0100

    MSBTS_Orchestration

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_Orchestration/MSBTS_Orchestration.ps1
@@ -0,0 +1,79 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_Orchestration WMI Class.                                               #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+# Get and Display Orchstrations not started
+[ARRAY]$orchs = Get-WmiObject MSBTS_Orchestration -namespace 'root\MicrosoftBizTalkServer' | Where-Object {$_.OrchestrationStatus -ne 4 }
+Write-Host "`nNot Started Orchestrations (" $orchs.Count ")" -fore DarkGray
+
+Foreach($orc in $orchs)
+{
+    Write-Host " - " $orc.Name
+}
+
+
+Function Start-Orchestration([string]$name)
+{
+    $orch_filter = "Name='$name'"
+    $orch = Get-WmiObject MSBTS_Orchestration -namespace 'root\MicrosoftBizTalkServer' -filter $orch_filter
+
+    $orch | ?{ $_.OrchestrationStatus -eq $bound } | %{
+        write-host "Enlisting $($_.Name)..."
+        [void]$_.Enlist()
+
+    }
+
+    $orch | ?{ $_.OrchestrationStatus -ne $started } | %{
+        write-host "Starting $($_.Name)..."
+        [void]$_.Start($controlRecvLoc, $controlInst)
+    }
+}
+
+Function Stop-Orchestration([string]$name, [switch]$unenlist = $false)
+{
+    $orch_filter = "Name='$name'"
+    $orch = $orch = Get-WmiObject MSBTS_Orchestration -namespace 'root\MicrosoftBizTalkServer' -filter $orch_filter
+
+    $orch | ?{ $_.OrchestrationStatus -eq $started } | %{
+        write-host "Stopping $($_.Name)..."
+        [void]$_.Stop($controlRecvLoc, $controlInst)
+        if ($unenlist){
+            [void]$_.Unenlist()
+        }
+    }
+}
+
+Function Unenlist-Orchestration([string]$name)
+{
+	$orch_filter = "Name='$name'"
+
+	try
+	{
+		$orchestration = get-wmiobject MSBTS_Orchestration -namespace 'root\MicrosoftBizTalkServer' -filter $orch_filter
+		
+		if($orchestration)
+		{
+			if ($orchestration.OrchestrationStatus -gt "2")
+			{
+				[void]$orchestration.Unenlist()
+			}
+		}
+		else
+		{
+			write-Host ""
+			write-host -foregroundcolor "yellow" "Could not find orchestration $name"
+			write-Host ""
+		}
+	}
+	catch
+	{
+		write-host -foregroundcolor "red" "$_.Exception.Message"
+		return $FALSE
+	}
+	return $TRUE	
+}
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_Orchestration/README.md
@@ -0,0 +1,7 @@
+# PowerShell to performing basic actions on the Orchestrations
+In this PowerShell sample how we can perform basic actions on the Orchestrations like: start, stop, unenlist or list orchestrations not started.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
