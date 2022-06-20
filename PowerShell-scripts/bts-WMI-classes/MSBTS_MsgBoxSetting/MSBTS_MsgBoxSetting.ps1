2442335ae3aef69afa27829c2a979b0e98e1241b
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-20 18:56:31 +0100

    MSBTS_MsgBoxSetting

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_MsgBoxSetting/MSBTS_MsgBoxSetting.ps1
@@ -0,0 +1,13 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_MsgBoxSetting WMI Class.                                               #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+$BizTalkMsgBoxDb = Get-WmiObject MSBTS_MsgBoxSetting -namespace root\MicrosoftBizTalkServer -ErrorAction Stop
+
+Write-Host "SQL Server Name: " $BizTalkMsgBoxDb.MsgBoxDBServerName
+Write-Host "BizTalk MessageBox Database: " $BizTalkMsgBoxDb.MsgBoxDBName
