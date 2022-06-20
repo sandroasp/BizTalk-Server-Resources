b13f104022730c052b67cd4633c020ed4970af38
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-20 23:29:19 +0100

    MSBTS_ReceiveLocation

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ReceiveLocation/MSBTS_ReceiveLocation.ps1
@@ -0,0 +1,72 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_ReceiveLocation WMI Class.                                             #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+########################################################################################################
+# Function to stop a receive location
+########################################################################################################
+Function Stop-Receive-Location($receiveLocationName)
+{
+	$name_filter = "Name='$receiveLocationName'"
+	
+	try {
+        $recvloc = Get-WmiObject MSBTS_ReceiveLocation -namespace 'root\MicrosoftBizTalkServer' -filter $name_filter
+		if ($recvloc)
+		{
+			[void]$recvloc.Disable()
+		}
+		else
+		{
+			write-host -foregroundcolor "yellow" "Could not find receive location $receiveLocationName"
+		}
+	}
+	catch
+	{
+		write-host -foregroundcolor "red" "$_.Exception.Message"	
+		return $FALSE
+	}
+	return $TRUE
+}
+
+########################################################################################################
+# Function to star receive location
+########################################################################################################
+Function Start-Receive-Location($receiveLocationName)
+{
+	$name_filter = "Name='$receiveLocationName'"
+
+	try
+	{
+		$recvloc = Get-WmiObject MSBTS_ReceiveLocation -namespace 'root\MicrosoftBizTalkServer' -filter $name_filter
+		if ($recvloc)
+		{
+			[void]$recvloc.Enable()
+		}
+		else
+		{
+			write-host -foregroundcolor "yellow" "Could not find receive location $receiveLocationName"
+		}
+	}		  
+	catch
+	{
+		write-host -foregroundcolor "red" "$_.Exception.Message"		
+		return $FALSE
+	}
+	return $TRUE
+}
+
+########################################################################################################
+# Function to display all information about a receive port
+########################################################################################################
+Function Display-Receive-Port-Locations($port)
+{
+   $portname = $port.Name
+   $recvlocs = Get-WmiObject MSBTS_ReceiveLocation -namespace ‘root\MicrosoftBizTalkServer’ -filter “ReceivePortName=’$portName'”
+
+   $recvlocs | ft Name, AdapterName, InboundTransportURL, IsDisabled
+}
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ReceiveLocation/README.md
@@ -0,0 +1,7 @@
+# PowerShell to Start, stop or list Receive locations
+IIn this PowerShell sample we are performing basic actions on the Receive Locations like: start, stop or list all receive locations from a specific receive port.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
