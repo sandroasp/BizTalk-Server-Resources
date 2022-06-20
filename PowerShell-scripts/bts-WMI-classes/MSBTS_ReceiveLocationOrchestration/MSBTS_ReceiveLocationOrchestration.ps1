440c2b0fe1d84b2b3878ae44220e8eb9ffdfd52d
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-20 23:44:33 +0100

    MSBTS_ReceiveLocationOrchestration

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ReceiveLocationOrchestration/MSBTS_ReceiveLocationOrchestration.ps1
@@ -0,0 +1,10 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_ReceiveLocationOrchestration WMI Class.                                #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+$recvlocs = Get-WmiObject MSBTS_ReceiveLocationOrchestration -namespace 'root\MicrosoftBizTalkServer' | where {$_.OrchestrationName -eq "POC_SB_AT.EDA" }
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ReceiveLocationOrchestration/README.md
@@ -0,0 +1,8 @@
+# PowerShell to list all receive locations binded to an Orchestration
+
+In this PowerShell sample we are getting a list of Receive Locations binded to a specific Orchestration.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
