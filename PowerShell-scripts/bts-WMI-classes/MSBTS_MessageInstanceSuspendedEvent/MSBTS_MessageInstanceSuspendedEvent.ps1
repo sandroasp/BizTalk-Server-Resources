bd80a7e9b6c244756aa057c392c8408b76fd147b
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-20 17:35:47 +0100

    MSBTS_MessageInstanceSuspendedEvent

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_MessageInstanceSuspendedEvent/MSBTS_MessageInstanceSuspendedEvent.ps1
@@ -0,0 +1,10 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_MessageInstanceSuspendedEvent WMI Class.                               #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+Get-WmiObject MSBTS_MessageInstanceSuspendedEvent -namespace 'root\MicrosoftBizTalkServer'
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_MessageInstanceSuspendedEvent/README.md
@@ -0,0 +1,7 @@
+# PowerShell sample of MSBTS_MessageInstanceSuspendedEvent
+In this PowerShell sample how we can use the MSBTS_MessageInstanceSuspendedEvent WMI class
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
