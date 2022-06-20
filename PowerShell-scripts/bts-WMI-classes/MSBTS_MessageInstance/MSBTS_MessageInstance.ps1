3410f1fa49e1c8973b51186918d0ea160da0eb0f
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-20 16:49:42 +0100

    MSBTS_MessageInstance

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_MessageInstance/MSBTS_MessageInstance.ps1
@@ -0,0 +1,32 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_MessageInstance WMI Class.                                             #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+
+########################################################################################################
+# Function to get all suspended messaging service instances both resumable and not-resumable
+########################################################################################################
+function BTS-Get-Messaging-Svc-Instances()
+{
+    Get-WmiObject MSBTS_ServiceInstance -namespace 'root\MicrosoftBizTalkServer' -filter 'ServiceClass=4 and (ServiceStatus = 4 or ServiceStatus = 16)'
+}
+
+########################################################################################################
+# Function to save the message associated to the specified messaging Service Instance
+########################################################################################################
+function BTS-Save-Message([string]$msgid)
+{
+    $path = "c:\temp"
+    $msg = Get-WmiObject MSBTS_MessageInstance -namespace 'root\MicrosoftBizTalkServer' -filter "ServiceInstanceID = '$msgid'"
+    $msg.psbase.invokemethod('SaveToFile', ($path))
+
+    Write-Host "Message from ServiceInstanceID=$msgid saved."
+}
+
+# save Suspended messsages
+BTS-Get-Messaging-Svc-Instances | %{ BTS-Save-Message($_.InstanceID) }
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_MessageInstance/README.md
@@ -0,0 +1,7 @@
+# Save Suspended messages with PowerShell
+In this PowerShell sample we can save all Suspended messages with PowerShell to a folder on our harddrive
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
