d4b8f02c540d81886927010783e989548d5c3430
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 00:03:31 +0100

    MSBTS_ReceivePort

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ReceivePort/MSBTS_ReceivePort.ps1
@@ -0,0 +1,19 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_ReceivePort WMI Class.                                                 #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+########################################################################################################
+# List all Receive Ports
+########################################################################################################
+Function List-Receive-Ports
+{
+   Get-WmiObject MSBTS_ReceivePort -namespace ‘root\MicrosoftBizTalkServer’
+}
+
+# Get all receive ports that as tracing enable
+$trackingRecPorts = Get-WmiObject MSBTS_ReceivePort -namespace 'root\MicrosoftBizTalkServer' | Where-Object {$_.Tracking -gt 0 }
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ReceivePort/README.md
@@ -0,0 +1,8 @@
+# PowerShell to interact with Receive Ports
+
+In this PowerShell sample we are getting a list all receive ports and/or all receive ports with somekind of tracing enable.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
