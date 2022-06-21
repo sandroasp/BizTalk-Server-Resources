9c44727696c07214304f49d45d4e333c031371b4
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 13:58:17 +0100

    MSBTS_TrackedMessageInstance2

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_TrackedMessageInstance2/MSBTS_TrackedMessageInstance2.cs
@@ -0,0 +1,21 @@
+using System.Management;  
+  
+//Sample to show how to save a tracked message out to a file folder using MSBTS_TrackedMessageInstance class  
+public void SaveTrackedMessageInstanceToFolder(Guid MessageInstanceId, string strOutputFolder)  
+{  
+    try  
+    {  
+    // Construct full WMI path to the MSBTS_TrackedMessageInstance using the message guid (NOTE: MessageInstanceID string value must be enclosed in {} curly brackets)  
+    string strInstanceFullPath = "root\\MicrosoftBizTalkServer:MSBTS_TrackedMessageInstance2.MessageInstanceID='{" + MessageInstanceId.ToString() + "}'";  
+
+    // Load the MSBTS_TrackedMessageInstance  
+    ManagementObject objTrackedSvcInst = new ManagementObject(strInstanceFullPath);  
+
+    // Invoke "SaveToFile" method to save the message out into the specified folder  
+    objTrackedSvcInst.InvokeMethod("SaveToFile", new object[] {strOutputFolder});  
+    }  
+    catch(Exception excep)  
+    {  
+    Console.WriteLine("Failed to save tracked message with id " + MessageInstanceId.ToString() + " into folder " + strOutputFolder + ": " + excep.Message);  
+    }  
+}  
