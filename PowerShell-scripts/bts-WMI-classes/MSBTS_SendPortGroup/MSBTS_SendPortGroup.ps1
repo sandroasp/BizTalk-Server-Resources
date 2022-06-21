6470f7aa0713e3dc194b75fff09a8aaae7f25e06
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 10:36:09 +0100

    MSBTS_SendPortGroup

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_SendPortGroup/MSBTS_SendPortGroup.ps1
@@ -0,0 +1,34 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_SendPortGroup WMI Class.                                               #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+########################################################################################################
+# Function to display all information about a send port group
+########################################################################################################
+function BTS-Display-Send-Port-Group($group)
+{
+    $groupname = $group.Name
+
+    $ports = Get-WmiObject MSBTS_SendPortGroup2SendPort -namespace 'root\MicrosoftBizTalkServer' -filter "SendPortGroupName='$groupName'"
+
+    Write-Host $groupname
+    foreach ($port in $ports) 
+    { 
+        "`t" + $port.SendPortName 
+    }
+}
+
+function BTS-List-Send-Port-Groups()
+{
+   Get-WmiObject MSBTS_SendPortGroup -namespace 'root\MicrosoftBizTalkServer'
+}
+
+function BTS-Get-Send-Port-Group([string] $name)
+{
+   Get-WmiObject MSBTS_SendPortGroup -namespace 'root\MicrosoftBizTalkServer' | Where-Object { $_.Name -match "$name" }
+}
