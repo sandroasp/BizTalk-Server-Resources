b70e4c610091bfe8e2c3e4a0ca9826358b2801d8
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 10:43:46 +0100

    MSBTS_SendPortGroup2SendPort

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_SendPortGroup2SendPort/MSBTS_SendPortGroup2SendPort.ps1
@@ -0,0 +1,24 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_SendPortGroup2SendPort  WMI Class.                                     #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+########################################################################################################
+# Function to display all information about a send port group
+########################################################################################################
+function BTS-Display-Send-Port-Group($group)
+{
+    $groupname = $group.Name
+
+    $ports = Get-WmiObject MSBTS_SendPortGroup2SendPort -namespace 'root\MicrosoftBizTalkServer' -filter "SendPortGroupName='$groupName'"
+
+    Write-Host $groupname
+    foreach ($port in $ports) 
+    { 
+        "`t" + $port.SendPortName 
+    }
+}
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_SendPortGroup2SendPort/README.md
@@ -0,0 +1,7 @@
+# PowerShell sample to use the MSBTS_SendPortGroup2SendPort WMI class
+In this PowerShell sample we are able to display all send ports that belongs to a specific send port group.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
