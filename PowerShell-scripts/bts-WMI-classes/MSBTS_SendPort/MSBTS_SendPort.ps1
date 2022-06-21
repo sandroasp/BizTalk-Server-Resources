15203ff4f7a20e02a60c77d87283aae8185e6399
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 10:14:00 +0100

    MSBTS_SendPort

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_SendPort/MSBTS_SendPort.ps1
@@ -0,0 +1,68 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_sendPort WMI Class.                                                    #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+function BTS-Set-Send-Port-Status($name, $status)
+{
+    $sendport = Get-WmiObject MSBTS_sendPort -namespace 'root\MicrosoftBizTalkServer' -filter "Name=’$name'"
+
+    switch ( $status )
+    {
+        'start' { 
+            $sendport.psbase.invokemethod('Start', $null)
+        }
+        'stop' { 
+            $sendport.psbase.invokemethod('Stop', $null)
+        }
+        'enlist' { 
+            $sendport.psbase.invokemethod('Enlist', $null) 
+        }
+        'unenlist' { 
+            $sendport.psbase.invokemethod('UnEnlist', $null) 
+        }
+    }
+}
+
+function BTS-List-Send-Ports()
+{
+   Get-WmiObject MSBTS_SendPort -namespace 'root\MicrosoftBizTalkServer'
+}
+
+function Start-Send-Port{
+    param([string]$portName)
+
+    $sp = Get-WmiObject MSBTS_SendPort -n root\MicrosoftBizTalkServer -filter "Name='$portName'"
+
+    if($sp -ne $null)
+    {
+        if($sp.Status -eq 1 -or $sp.Status -eq 2)
+        {
+            if($sp.Status -eq 1)
+            {
+                $null = $sp.Enlist()
+            }
+            $null = $sp.Start()
+            Write-Host "Started send port: " + $portName -fore Green
+        }
+        else
+        {
+            Write-Host "Send port " + $portName + " is already started." -fore Yellow
+        }
+    }
+    else
+    {
+        Write-Host "Send port not found" -fore Red
+    }
+}
+
+#Get Counters: Check if there are any disable receive locations and stopped/unelisted send ports
+if ($sendports.count -ne (get-wmiobject MSBTS_SendPort -namespace 'root\MicrosoftBizTalkServer' -filter '(Status = 3)').count)
+{
+    [BOOL]$script:Continuescript = $True
+    [INT]$countSendPorts = $sendports.count - $(get-wmiobject MSBTS_SendPort -namespace 'root\MicrosoftBizTalkServer' -filter {Status = 3}).count
+}
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_SendPort/README.md
@@ -0,0 +1,7 @@
+# PowerShell sample to use the MSBTS_SendPort WMI class
+In this PowerShell sample we are able to count how many send ports are stopped and start a specific send port and many other operations.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
