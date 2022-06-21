5769eff92b60e19a3ce7ac48397e8c1fdb85a183
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 11:01:19 +0100

    MSBTS_Server

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_Server/MSBTS_Server.ps1
@@ -0,0 +1,11 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_Server WMI Class.                                                      #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+$BizTalkServer = Get-WmiObject MSBTS_Server -namespace root\MicrosoftBizTalkServer -ErrorAction Stop
+Write-Host "Server name:" $BiztalkServer.Name
