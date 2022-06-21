5a5c275d06e72c1a1a3648f54191b4a85d7fd118
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 12:10:11 +0100

    MSBTS_ServiceInstanceSuspendedEvent

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ServiceInstanceSuspendedEvent/MSBTS_ServiceInstanceSuspendedEvent.ps1
@@ -0,0 +1,10 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_ServiceInstanceSuspendedEvent WMI Class.                               #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+$sinstance = Get-WmiObject MSBTS_ServiceInstanceSuspendedEvent -namespace 'root\MicrosoftBizTalkServer'
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ServiceInstanceSuspendedEvent/README.md
@@ -0,0 +1,7 @@
+# PowerShell sample to use the MSBTS_ServiceInstanceSuspendedEvent WMI class
+In this PowerShell sample we are getting a list of all suspended events.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
