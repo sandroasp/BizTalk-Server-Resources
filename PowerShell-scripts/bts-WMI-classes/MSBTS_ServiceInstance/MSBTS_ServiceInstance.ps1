a23e9ae70a95c6f6f99d59cba15dc26b45e33a77
Sandro Pereira <sandro-pereira@live.com.pt>
2022-06-21 11:53:23 +0100

    MSBTS_ServiceInstance

---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ServiceInstance/MSBTS_ServiceInstance.ps1
@@ -0,0 +1,20 @@
+﻿########################################################################################################
+#                                                                                                      #
+# Author: Sandro Pereira                                                                               #
+#                                                                                                      #
+# Description: Use of the MSBTS_ServiceInstance WMI Class.                                             #
+#                                                                                                      #
+#                                                                                                      #
+########################################################################################################
+
+
+########################################################################################################
+# Function to get all suspended messaging service instances both resumable and not-resumable
+########################################################################################################
+function BTS-Get-Messaging-Svc-Instances()
+{
+    Get-WmiObject MSBTS_ServiceInstance -namespace 'root\MicrosoftBizTalkServer' -filter 'ServiceClass=4 and (ServiceStatus = 4 or ServiceStatus = 16)'
+}
+
+# list Suspended messages
+BTS-Get-Messaging-Svc-Instances | fl InstanceId, ServiceName, SuspendTime, HostName, ServiceStatus, ErrorId, ErrorDescription
---
+++ PowerShell-scripts/bts-WMI-classes/MSBTS_ServiceInstance/README.md
@@ -0,0 +1,7 @@
+# PowerShell sample to use the MSBTS_ServiceInstance WMI class
+In this PowerShell sample we are getting all suspended messaging service instances both resumable and not-resumable.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
