6c21c9af88c94c0e98f985446e188d3ecf2ab4a3
Sandro Pereira <sandro-pereira@live.com.pt>
2025-10-30 13:13:53 +0000

    alternative

---
+++ PowerShell-scripts/adm-in-which-Event-Log-belongs-specific-source/adm-in-which-Event-Log-belongs-specific-source-registry.ps1
@@ -0,0 +1,7 @@
+﻿$src = 'BizTalk Server'
+Get-ChildItem 'HKLM:\SYSTEM\CurrentControlSet\Services\EventLog' -Recurse -ErrorAction SilentlyContinue |
+Where-Object {
+  $_.PSChildName -eq $src -or
+  ((Get-ItemProperty $_.PsPath -ErrorAction SilentlyContinue).Sources -contains $src)
+} |
+Select-Object @{n='LogName';e={ Split-Path $_.PSParentPath -Leaf }} -Unique
