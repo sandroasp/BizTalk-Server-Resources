ed765fe877342092ef576d417b3cd7b4f3c1d496
Sandro Pereira <sandro-pereira@live.com.pt>
2025-11-17 12:20:29 +0000

    PowerShell to Export BizTalk Applications resources to an XML file

---
+++ PowerShell-scripts/adm-bts-Export/Export-BizTalk-Applications-resources-to XML.ps1
@@ -0,0 +1,16 @@
+﻿$app     = 'my-app'
+$spec    = 'C:\BizTalkApplications\app\MyArtifacts.xml'
+
+# Find BTSTask.exe in a dynamic way
+$btstask = Get-ChildItem `
+  'C:\Program Files*' `
+  -Filter 'BTSTask.exe' -Recurse -ErrorAction SilentlyContinue `
+  | Select-Object -First 1 -ExpandProperty FullName
+
+if (-not $btstask) { throw "BTSTask.exe not found. Is BizTalk Server installed in this machine?" }
+$btstask
+
+
+# Run and show output in console
+& $btstask 'ListApp' "/ApplicationName:$app" "/ResourceSpec:$spec"
+if ($LASTEXITCODE -ne 0) { throw "BTSTask failed with exit code $LASTEXITCODE" }
---
+++ PowerShell-scripts/adm-bts-Export/README.md
@@ -0,0 +1,7 @@
+# PowerShell to Export BizTalk Applications resources to an XML file
+This is a PowerShell script that calls BTSTask command (handles spaces, captures output, and checks exit code) in order to export a list of all resources opf the application to an XML files.
+
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
